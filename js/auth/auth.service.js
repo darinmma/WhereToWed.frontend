5938ecd75b6c4084e13aca367857a6b5d3e42f79
Darin Ma <darinmma@gmail.com>
2016-12-15 13:01:52 -0800

    change paths to heroku backend

--- js/auth/auth.service.js
+++ js/auth/auth.service.js
@@ -24,7 +24,7 @@
     function logIn(data) {
       var promise = $http({
         method: 'POST',
-        url:    'http://localhost:3000/api/token',
+        url:    'https://wheretowed.herokuapp.com/api/token',
         data:   data
       })
       .then(
